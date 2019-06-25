4f87cc847f9b2f3d735dfbdc6888a23c91eaae5b
CBJlove11 <2466152968@qq.com>
2019-06-25 11:58:43 +0800

    GG

---
+++ app/src/main/java/com/example/yibandemo/activity/UserActivity.java
@@ -0,0 +1,15 @@
+package com.example.yibandemo.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yibandemo.R;
+
+public class UserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
